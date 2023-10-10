1446320ba9141494aa93c98c9afea75dc60b7086
FlipSiteX <verapushtrinova@gmail.com>
2023-10-11 02:24:04 +0300

    картинки почти рандомные

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let result = document.getElementById('result')
 const randomNumbersBlock = document.getElementsByClassName('random_numbers')
 const randomImagesBlock = document.getElementsByClassName('random_images')
 let imgPlace = document.getElementsByClassName('image_place')
-let imgArray = ["../img/1.png", "../img/2.png", "../img/3.png"]
+let imgArray = ["https://fikiwiki.com/uploads/posts/2022-02/1644874866_27-fikiwiki-com-p-klassnie-kartinki-dlya-vk-27.jpg", "https://get.wallhere.com/photo/cat-face-glasses-thick-1047364.jpg", "https://w-dog.ru/wallpapers/1/16/453860014338874/bmw-m5-staraya-bmv-angelskie-glazki-avto-foto-tachki-avto-oboi-avtomobili.jpg"]
 
 let randomNumber = () => {
     const minValue = +document.getElementById('startCount').value
